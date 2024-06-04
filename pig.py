bd1ead796319efcb15b08884fcf0b2bc78ef3f2b
Enoch Flourish <flourzeez@gmail.com>
2024-06-04 11:20:13 +0300

    making little chnages

--- pig.py
+++ pig.py
@@ -1,7 +1,7 @@
 import random
 
-def roll (): # this is to get the random number for the dice
-    
+def roll():
+    # This is to get the random number for the dice
     min_num = 1
     max_num = 6
 
@@ -9,39 +9,56 @@ def roll (): # this is to get the random number for the dice
     return roll
 
 
-while True: #this is to get the number of players
+while True:
+    # This is to get the number of players
+    players = input("Enter the number of players (2 - 4): ")
+    # Ask the user to enter the number of players
 
-    players = input ("Enter the number of players (2 - 4): ")
-    if players.isdigit(): #check if the input is a digit
-        players = int(players) #convert the input to an integer
-        if 2 <= players <= 4: #check if the input is within the range
-            break #exit the loop
-        else:   #if the input is not within the range
-            print ("Please enter a number between 1 and 4")     #print an error message
+    if players.isdigit():
+        # Check if the input is a digit
+        players = int(players)
+        # Convert the input to an integer
 
-print ("The number of players are: ", players)
+        if 2 <= players <= 4:
+            # Check if the input is within the range
+            break
+            # Exit the loop
+        else:
+            # If the input is not within the range
+            print("Please enter a number between 1 and 4")
+            # Print an error message
+
+print("The number of players are: ", players)
 
 max_score = 100
-players_scores = [0 for _ in range(players)] #this creates a list of zeros for each player
+players_scores = [0 for _ in range(players)]
+# This creates a list of zeros for each player
 
-print ("The scores are: ", players_scores)
+print("The scores are: ", players_scores)
 
 while max(players_scores) < max_score:
-    current_score = 0 
 
-    print ("Player 1: ", players_scores[0])
-    print ("Player 2: ", players_scores[1])
+    for i in range(players):
+        current_score = 0
+        
+    while True:
+        should_continue = input("Do you want to roll the dice (y)?: ")
+        if should_continue.lower() != "y":
+            break
 
-    should_continue = input ("Do you want to roll the dice (y)?: ")
-    if should_continue.lower() != "y":
-         break
-    else:
         value = roll()
-    if value == 1:
-        print ("You rolled a 1. You lose all of  your points")
-    else:
-        current_score += value
-        print ("You rolled a: ", value)
+
+        if value == 1:
+            print("You rolled a 1. You lose all of your points")
+            break
+        else:
+            current_score += value
+            print("You rolled a: ", value)
+
+
+ 
+
+   
 
 
    
